975bdcaaa8339c71271af199591322221c466996
Ravi Sharma <Ravi_kanchan2004@yahoo.com>
2026-07-23 13:43:46 +0530

    final sync

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,8 @@
-resource "azurerm_resource_group" "this" {
- name     = var.resource_group_name
- location = var.location
- tags     = var.tags
-}
+#resource "azurerm_resource_group" "this" {
+# name     = var.resource_group_name
+# location = var.location
+# tags     = var.tags
+#}
 
 resource "azurerm_virtual_network" "this" {
  name                = var.vnet_name
